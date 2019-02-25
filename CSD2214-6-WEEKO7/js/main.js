1a21d66d2e3f5b32e6396f5be2a6136008939bae
visualstudio <visualstudio@230-PC33>
2019-02-25 15:31:14 -0500

    3:30

--- CSD2214-6-WEEKO7/js/main.js
+++ CSD2214-6-WEEKO7/js/main.js
@@ -1,1 +1,26 @@
-﻿// Your code here!
+﻿var bird = {
+    genius: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+
+
+};
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonNmae: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: "true",
+    deadly: "true"
+
+
+
+};
